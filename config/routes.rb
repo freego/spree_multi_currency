3881ed4207ecb57d4be6f46a4ce8794ad3fd62ed
Alessandro Lepore <lepore.ale@gmail.com>
2013-11-12 18:22:05 +0100

    fix routes for rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Spree::Core::Engine.routes.draw do
-  match '/currency/set', :to => 'currency#set', :defaults => { :format => :json }, :as => :set_currency
+  match '/currency/set', to: 'currency#set', defaults: { format: :json },
+                         as: :set_currency, via: [:get, :post]
 
   namespace :admin do
     resources :products do
-      resources :prices, :only => [:index, :create]
+      resources :prices, only: [:index, :create]
     end
   end
 end
